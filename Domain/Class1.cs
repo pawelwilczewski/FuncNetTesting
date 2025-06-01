4dbaea81f5a5472ea503de57c157e4dca4f5392d
Pawel Wilczewski <pawel.wilczewski@outlook.com>
2025-06-01 11:05:45 +0100

    Initial testing setup

---
+++ Domain/Class1.cs
@@ -0,0 +1,20 @@
+﻿using FuncNet;
+
+namespace Domain;
+
+public class Class1
+{
+	public static Union<int, string, bool> Value { get; set; } = 32;
+
+	private Option<string> test = Option<string>.FromNullable("abcd");
+
+	public void Method()
+	{
+		Value = Union<string, int>.FromT0("abc");
+		Value = 43;
+		Value = Union<bool, int, string>.FromT1(32);
+		Value = false;
+	}
+}
+
+// Union<int, bool, string>
--- FuncNetTesting/Program.cs
+++ FuncNetTesting/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Domain;
+using FuncNet;
+
 Console.WriteLine("Hello, World!");
+
+Class1.Value.Map0(number => $"hello{number}");
+
+Class1.Value = true;
+
+Class1.Value = Union<int, string, bool>.FromT0(43);
+
+Class1.Value = Union<int, string>.FromT0(43);
+
+Class1.Value = Union<int, bool, string>.FromT0(43);
+
+Union<int, bool, string> test = Union<bool, int, string>.FromT2("hello");
