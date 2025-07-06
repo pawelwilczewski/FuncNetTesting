fab064ddbf793763e8a726ed3fd26f48e9841cc4
Pawel Wilczewski <pawel.wilczewski@outlook.com>
2025-07-06 02:55:44 +0100

    This works correctly

--- FuncNetTesting/Program.cs
+++ FuncNetTesting/Program.cs
@@ -15,3 +15,11 @@ Class1.Value = Union<int, bool, string>.FromT0(43);
 
 Union<int, bool, string> test = Union<bool, int, string>.FromT2("hello");
 Union<int, string, bool, DateTime> test2 = test;
+
+Union<bool, decimal, float, int> newUnion = 132;
+Union<decimal, float, bool, int> union2 = 432;
+newUnion = union2;
+
+Result<int, float, decimal, bool, DateTime, string, double> abc1 = true;
+Result<int, float, decimal, bool, DateTime, double, string> abc2 = "abbbb";
+abc1 = abc2;
